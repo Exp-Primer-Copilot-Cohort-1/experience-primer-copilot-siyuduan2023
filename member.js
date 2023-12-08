97c15948c6ff13142920977361e20e518835d7d8
Siyu <siyusiyuduan@hotmail.com>
2023-12-08 19:32:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var projectsContent = document.getElementById("projectsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutContent");
+    memberContent.style.display = "block";
+    skillsContent.style.display = "none";
+    projectsContent.style.display = "none";
+    contactContent.style.display = "none";
+    aboutContent.style.display = "none";
+    member.style.backgroundColor = "#333";
+    skills.style.backgroundColor = "#111";
+    projects.style.backgroundColor = "#111";
+    contact.style.backgroundColor = "#111";
+    about.style.backgroundColor = "#111";
+}
